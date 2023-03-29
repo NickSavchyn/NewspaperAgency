e9af1b7b87b0544836de140283dd90c0eb139bf1
Mykola Savchyn <kola.savchyn@gmail.com>
2023-03-29 19:40:40 +0300

    Add views

--- newspaper_agency/views.py
+++ newspaper_agency/views.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from newspaper_agency.models import Topic, Newspaper, Redactor
+
+
+@login_required
+def index(request):
+    """View function for the home page of th site"""
+
+    num_topics = Topic.objects.count()
+    num_newspapers = Newspaper.objects.count()
+    new_redactors = Redactor.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.sesion["num_visits"] = num_visits + 1
+
+    context = {
+        "num_topics": num_topics,
+        "num_newspapers": num_newspapers,
+        "new_redactors": new_redactors,
+        "num_visits": num_visits + 1
+    }
+
+    return render(request, "newspaper_agency/index.html", context=context)
